d146a9b76a8dcc97963d6b03d1c0fa31c2e3bd72
ClarkeEl360 <ClarkeEl360@northwested.org>
2024-09-16 10:37:19 -0400

    Create mad_code.py

---
+++ mad_code.py
@@ -0,0 +1,17 @@
+print("Choose 4 noons.")
+noon = input()
+noon1 = input()
+noon2 = input()
+noon3 = input()
+
+print("Choose 1 adjectives.")
+adj = input()
+
+print("Choose 1 games.")
+game = input()
+
+print("Choose 2 foods")
+food = input()
+food1 = input()
+
+print(f"Today I went to the {noon.title()}, where I played {game}, and ate {food}. I then desided to go (to) {noon1}, where I {adj}, with (my) {noon2}, and then finaly I went home, ate some {food1}, and went to my {noon3}.")
